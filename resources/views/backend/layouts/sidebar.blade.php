8ef6d6c2f11d474443a4c84749491e84a041e35b
KareemShaban1 <karim17196@fci.bu.edu.eg>
2026-01-25 12:15:24 +0200

    change sidebar slogan in dashboard

--- resources/views/backend/layouts/sidebar.blade.php
+++ resources/views/backend/layouts/sidebar.blade.php
@@ -4,7 +4,7 @@
   	<!-- LOGO -->
   	<a class="logo text-center" href="#">
   		<span class="logo-lg">
-  			<i class="fas fa-laptop-code"></i> <span class="logo-text">KAREEM SOFT</span>
+  			<i class="fas fa-laptop-code"></i> <span class="logo-text">Quantum Technical</span>
   		</span>
   		<span class="logo-sm">
   			<i class="fas fa-laptop-code"></i>
@@ -14,11 +14,11 @@
   	<!-- LOGO -->
   	<a class="logo text-center logo-dark">
   		<span class="logo-lg text-white">
-  			kareem soft
+  			Quantum Technical
   			<!-- <img src="{{asset('backend/assets/images/logo-dark.png')}}" alt="" height="16"> -->
   		</span>
   		<span class="logo-sm text-white">
-  			kareem soft
+  			Quantum Technical
   			<!-- <img src="{{asset('backend/assets/images/logo_sm_dark.png')}}" alt="" height="16"> -->
   		</span>
   	</a>
@@ -30,72 +30,73 @@
 
 
 
-            <!-- CMS Menu -->
-            <li class="side-nav-title side-nav-item">{{__('CMS')}}</li>
+  			<!-- CMS Menu -->
+  			<li class="side-nav-title side-nav-item">{{__('CMS')}}</li>
 
-            <li class="side-nav-item">
-                <a href="{{ route('cms.languages.index') }}" class="side-nav-link">
-                    <i class="uil-globe"></i>
-                    <span>{{__('Languages')}}</span>
-                </a>
-            </li>
+  			<li class="side-nav-item">
+  				<a href="{{ route('cms.languages.index') }}" class="side-nav-link">
+  					<i class="uil-globe"></i>
+  					<span>{{__('Languages')}}</span>
+  				</a>
+  			</li>
 
-            <li class="side-nav-item">
-                <a data-bs-toggle="collapse" href="#sidebarCmsPages" aria-expanded="false" aria-controls="sidebarCmsPages" class="side-nav-link">
-                    <i class="uil-file-alt"></i>
-                    <span> {{__('Pages')}} </span>
-                    <span class="menu-arrow"></span>
-                </a>
-                <div class="collapse" id="sidebarCmsPages">
-                    <ul class="side-nav-second-level">
-                        <li>
-                            <a href="{{ route('cms.pages.index') }}">
-                                <span>{{__('All Pages')}}</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="{{ route('cms.pages.create') }}">
-                                <span>{{__('Add Page')}}</span>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </li>
+  			<li class="side-nav-item">
+  				<a data-bs-toggle="collapse" href="#sidebarCmsPages" aria-expanded="false"
+  					aria-controls="sidebarCmsPages" class="side-nav-link">
+  					<i class="uil-file-alt"></i>
+  					<span> {{__('Pages')}} </span>
+  					<span class="menu-arrow"></span>
+  				</a>
+  				<div class="collapse" id="sidebarCmsPages">
+  					<ul class="side-nav-second-level">
+  						<li>
+  							<a href="{{ route('cms.pages.index') }}">
+  								<span>{{__('All Pages')}}</span>
+  							</a>
+  						</li>
+  						<li>
+  							<a href="{{ route('cms.pages.create') }}">
+  								<span>{{__('Add Page')}}</span>
+  							</a>
+  						</li>
+  					</ul>
+  				</div>
+  			</li>
 
-            <li class="side-nav-item">
-                <a href="{{ route('cms.sections.index') }}" class="side-nav-link">
-                    <i class="uil-layers"></i>
-                    <span>{{__('Sections')}}</span>
-                </a>
-            </li>
+  			<li class="side-nav-item">
+  				<a href="{{ route('cms.sections.index') }}" class="side-nav-link">
+  					<i class="uil-layers"></i>
+  					<span>{{__('Sections')}}</span>
+  				</a>
+  			</li>
 
-            <li class="side-nav-item">
-                <a href="{{ route('cms.items.index') }}" class="side-nav-link">
-                    <i class="uil-list-ul"></i>
-                    <span>{{__('Items')}}</span>
-                </a>
-            </li>
+  			<li class="side-nav-item">
+  				<a href="{{ route('cms.items.index') }}" class="side-nav-link">
+  					<i class="uil-list-ul"></i>
+  					<span>{{__('Items')}}</span>
+  				</a>
+  			</li>
 
-            <li class="side-nav-item">
-                <a href="{{ route('cms.links.index') }}" class="side-nav-link">
-                    <i class="uil-link"></i>
-                    <span>{{__('Links')}}</span>
-                </a>
-            </li>
+  			<li class="side-nav-item">
+  				<a href="{{ route('cms.links.index') }}" class="side-nav-link">
+  					<i class="uil-link"></i>
+  					<span>{{__('Links')}}</span>
+  				</a>
+  			</li>
 
-            <li class="side-nav-item">
-                <a href="{{ route('cms.media.index') }}" class="side-nav-link">
-                    <i class="uil-image"></i>
-                    <span>{{__('Media Library')}}</span>
-                </a>
-            </li>
+  			<li class="side-nav-item">
+  				<a href="{{ route('cms.media.index') }}" class="side-nav-link">
+  					<i class="uil-image"></i>
+  					<span>{{__('Media Library')}}</span>
+  				</a>
+  			</li>
 
-            <li class="side-nav-item">
-                <a href="{{ route('cms.translations.index') }}" class="side-nav-link">
-                    <i class="uil-language"></i>
-                    <span>{{__('Translations')}}</span>
-                </a>
-            </li>
+  			<li class="side-nav-item">
+  				<a href="{{ route('cms.translations.index') }}" class="side-nav-link">
+  					<i class="uil-language"></i>
+  					<span>{{__('Translations')}}</span>
+  				</a>
+  			</li>
 
   		</ul>
 
